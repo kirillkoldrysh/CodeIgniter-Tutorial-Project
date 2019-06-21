72c71a58fb656cae0222e08da0554af8b2bbaef6
BadKoldrysh <ohmygodkir17@gmail.com>
2019-06-21 21:19:30 +0200

    Add functionality to Cities model

--- application/models/Cities.php
+++ application/models/Cities.php
@@ -1,2 +1,10 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
+
+class Cities extends BaseModel {
+    protected $table = 'cities';
+
+    public function __construct() {
+        parent::__construct();
+    }
+}
