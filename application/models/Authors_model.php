153710f4e84f1758a2ad581cfbe99e574fd3985c
BadKoldrysh <ohmygodkir17@gmail.com>
2019-06-24 20:49:13 +0200

    Add 'Authors_model.php' with new model. Modify database name

---
+++ application/models/Authors_model.php
@@ -0,0 +1,16 @@
+<?php
+
+class Authors_model extends CI_Model {
+    protected $table = 'authors';
+
+    public function get_count() {
+        return $this->db->count_all($this->table);
+    }
+
+    public function get_authors($limit, $start) {
+        $this->db->limit($limit, $start);
+        $query = $this->db->get($this->table);
+        
+        return $query->result();
+    }
+}
